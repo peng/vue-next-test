d4cbcac5dd397c1430509e0c1d63220660b03eb1
peng <414326615@qq.com>
2020-01-09 10:47:11 +0800

    init

---
+++ src/js/component/render.js
@@ -0,0 +1,20 @@
+import { mount } from './mount';
+import { patch } from './patch';
+
+export function render(vnode, container) {
+  const prevVNode = container.vnode
+  if (prevVNode == null) {
+    if (vnode) {
+      mount(vnode, container)
+      container.vnode = vnode
+    }
+  } else {
+    if (vnode) {
+      patch(prevVNode, vnode, container)
+      container.vnode = vnode
+    } else {
+      container.removeChild(prevVNode.el)
+      container.vnode = null
+    }
+  }
+}
